f74d975bff697ed13256d507dcf7dae8062686c0
DavidStewartSB <87091732+DavidStewartSB@users.noreply.github.com>
2021-11-12 18:33:22 -0300

    Web-NodeJs-ReactJS
    Update2

--- tarefa-backend/backend/src/index.ts
+++ tarefa-backend/backend/src/index.ts
@@ -2,15 +2,14 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from 'cors';
 import routes from "./routes";
-
+ 
 const app = express()
-
+createConnection()
+ 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
-createConnection()
-
-
-app.listen(3333 , () => {
-    console.log("Servidor Backend ON!!!")
-})
+ 
+app.listen(3333)
